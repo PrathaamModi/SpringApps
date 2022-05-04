7a0aea31cf6befe101180d442cce5f5a827bcba6
Prathaam Modi <pmodi@teksystems.com>
2022-05-04 10:11:57 +0545

    sql jdbc intro

---
+++ 02springjdbc/src/test/java/com/springapps/springjdbc/SpringJdbcApp.java
@@ -0,0 +1,18 @@
+package com.springapps.springjdbc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class SpringJdbcApp {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springapps/springjdbc/config.xml");
+		JdbcTemplate jdbctemplate = (JdbcTemplate) context.getBean("jdbctemplate");
+		String sql="insert into employee values(?,?,?)";
+		int result = jdbctemplate.update(sql, 1, "John", "Wilson");
+		System.out.println("Number of records inserted are : " + result);
+	}
+
+}
