530bdc1947dc46b511efb9d80350fa68aa30d87b
Prathaam Modi <pmodi@teksystems.com>
2022-05-04 10:51:54 +0545

    sql jdbc table insertion using template

--- 02springjdbc/src/main/java/com/springapps/springjdbc/employee/app/SpringJdbcApp.java
+++ 02springjdbc/src/main/java/com/springapps/springjdbc/employee/app/SpringJdbcApp.java
@@ -1,18 +1,29 @@
-package com.springapps.springjdbc;
+package com.springapps.springjdbc.employee.app;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import com.springapps.springjdbc.employee.dao.EmployeeDao;
+import com.springapps.springjdbc.employee.dto.Employee;
+
 public class SpringJdbcApp {
 
 	public static void main(String[] args) {
-
-		ApplicationContext context = new ClassPathXmlApplicationContext("com/springapps/springjdbc/config.xml");
-		JdbcTemplate jdbctemplate = (JdbcTemplate) context.getBean("jdbctemplate");
-		String sql="insert into employee values(?,?,?)";
-		int result = jdbctemplate.update(sql, 1, "John", "Wilson");
-		System.out.println("Number of records inserted are : " + result);
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springapps/springjdbc/employee/config/config.xml");
+		EmployeeDao employeedao = (EmployeeDao) context.getBean("employeedao");
+		
+		createEmployee(employeedao);
+		
 	}
 
+	private static void createEmployee(EmployeeDao employeedao) {
+		Employee emp = new Employee();
+		emp.setId(2);
+		emp.setFirstName("Max");
+		emp.setLastName("Rider");
+		int result = employeedao.create(emp);
+		System.out.println("Number of records inserted are: " + result);
+	}
+	
 }
